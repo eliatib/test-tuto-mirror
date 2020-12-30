c67c7bf3708b4aa25a7e3cbbb23303c4360b0397
Thibault Michel <thibault.michel@viacesi.fr>
2020-12-30 15:50:02 +0100

    Test mirror based on Tuto Unity FR Channel (https://www.youtube.com/watch?v=65i-egqXY0o&list=PLUWxWDlz8PYI2OdmIhOnxg92kl0DgS1EN)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+[RequireComponent(typeof(ConfigurableJoint))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float m_speed = 3f;
+
+    [SerializeField]
+    private float m_rotationSpeedX = 3f;
+
+    [SerializeField]
+    private float m_rotationSpeedY = 3f;
+
+    [SerializeField]
+    private float thrusterForce = 1000f;
+
+    [Header("Joints Options")]
+
+    [SerializeField]
+    private float jointSpring = 20f;
+
+    [SerializeField]
+    private float jointMaxForce = 50f;
+
+    private PlayerMotor motor;
+    private ConfigurableJoint joint;
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        motor = GetComponent<PlayerMotor>();
+        joint = GetComponent<ConfigurableJoint>();
+        setJointSettings(jointSpring);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * horizontal;
+        Vector3 moveVertical = transform.forward * vertical;
+        Vector3 thrusterVelocity = Vector3.zero;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * m_speed;
+
+        motor.Move(velocity);
+
+        //rotation
+        float yRot = Input.GetAxisRaw("Mouse X");
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 rotation = new Vector3(0, yRot, 0) * m_rotationSpeedX;
+
+        float cameraRotationX = xRot * m_rotationSpeedY;
+
+        motor.Rotate(rotation);
+        motor.RotateCamera(cameraRotationX);
+
+        if (Input.GetButton("Jump")) 
+        {
+            thrusterVelocity = Vector3.up * thrusterForce;
+            setJointSettings(0f);
+        }
+        else 
+        {
+            setJointSettings(jointSpring);
+        }
+
+        motor.ApplyThruster(thrusterVelocity);
+    }
+
+    private void setJointSettings(float _jointSpring) 
+    {
+        joint.yDrive = new JointDrive { positionSpring = _jointSpring, maximumForce = jointMaxForce };
+    }
+
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity;
+    private Vector3 rotation;
+    private float cameraRotationX = 0F;
+    private float currentCameraRotationX = 0F;
+    private Vector3 thrusterVelocity;
+    private Rigidbody rb;
+
+
+    [SerializeField]
+    private float cameraRotationLimit = 85f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(float _cameraRotationX)
+    {
+        cameraRotationX = _cameraRotationX;
+    }
+
+    public void ApplyThruster(Vector3 _thrusterVelocity)
+    {
+        thrusterVelocity = _thrusterVelocity;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    private void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+
+        if(thrusterVelocity != Vector3.zero) 
+        {
+            rb.AddForce(thrusterVelocity * Time.fixedDeltaTime, ForceMode.Acceleration);
+        }
+    }
+
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        currentCameraRotationX += cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX,-cameraRotationLimit,cameraRotationLimit);
+        cam.transform.localEulerAngles = new Vector3(-currentCameraRotationX, 0F, 0f);
+    }
+}
